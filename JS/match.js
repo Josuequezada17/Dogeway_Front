18ea0d493f7faadcdc3a750f15201011b5a03fd5
PabloLuna44 <guzmanlunajuanpablo@gmail.com>
2023-11-21 14:24:33 -0600

    Conexion Login

--- JS/match.js
+++ JS/match.js
@@ -1,40 +1,11 @@
-function match(CurrentUser,MatchUser,status){
+function match(){
 
-    const apiUrl = `http://localhost:8080/match/IsAMatch`;
+    
 
-
-    fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-           
-        },
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error de red: ${response.status} - ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Respuesta del servidor:exitosa',data);
-            var contador=localStorage.getItem('paginacion');
-            contador++;
-            localStorage.setItem('paginacion',contador);
-            window.location.href = 'match.html';
-
-            
-
-        })
-        .catch(error => {
-            console.error('Error al recuperar información:', error);
-            // Manejar el error de manera adecuada, por ejemplo, redirigir a una página de error
-
-        });
-
-
-
+    var contador=localStorage.getItem('paginacion');
+    contador++;
+    localStorage.setItem('paginacion',contador);
+    window.location.href = 'match.html';
 
     }
 
@@ -44,42 +15,11 @@ function match(CurrentUser,MatchUser,status){
 
 function NotMatch(){
 
- const apiUrl = `http://localhost:8080/match/NotMatch`;
-
-
- fetch(apiUrl, {
-     method: 'GET',
-     headers: {
-         'Authorization': token,
-         'Content-Type': 'application/json'
-        
-     },
- })
-     .then(response => {
-         if (!response.ok) {
-             throw new Error(`Error de red: ${response.status} - ${response.statusText}`);
-         }
-         return response.json();
-     })
-     .then(data => {
-         console.log('Respuesta del servidor:exitosa',data);
-         var contador=localStorage.getItem('paginacion');
-         contador++;
-         localStorage.setItem('paginacion',contador);
-         window.location.href = 'match.html';
-         
-         
-
-     })
-     .catch(error => {
-         console.error('Error al recuperar información:', error);
-         // Manejar el error de manera adecuada, por ejemplo, redirigir a una página de error
-
-     });
-
-
-
-
+ 
+    var contador=localStorage.getItem('paginacion');
+    contador++;
+    localStorage.setItem('paginacion',contador);
+    window.location.href = 'match.html';
  
 
 
