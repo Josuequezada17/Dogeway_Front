f64697d3e2b87533e61e6d5bcd26ad6409339c27
PabloLuna44 <guzmanlunajuanpablo@gmail.com>
2023-11-25 22:17:55 -0600

    chat

--- JS/chat.js
+++ JS/chat.js
@@ -94,12 +94,11 @@ function ListAllPets(correo, token) {
                         imgElement.addEventListener('click', function () {
                             sessionStorage.setItem('paginacion',0);
                             sessionStorage.setItem('currentPet', JSON.stringify(mascota));
-                            sessionStorage.setItem('userPropietary',JSON.stringify(mascota.userResponseDTO));
-                            var currentPet = JSON.parse(sessionStorage.getItem('currentPet'));
-                            ListOfUsers(sessionStorage.token,currentPet.animal,!currentPet.genero,currentPet.tamano,sessionStorage.paginacion);
+                            
+                            loadMatchs();
                             alert('La mascota ha sido cambiada por ' + mascota.nombre);
                             
-                            window.location.href = 'adopcionesPendientes.html';
+                            window.location.href = 'chat.html';
                         });
             
                         celda.appendChild(imgElement);
@@ -135,7 +134,7 @@ function ListAllPets(correo, token) {
 
 
 function loadMatchs() {
-    const sidebar = document.getElementById('sidebar');
+    
 
     // Obtener el token de autenticación de donde lo tengas almacenado en tu aplicación 
     const user=JSON.parse(sessionStorage.getItem('AllDataUser'));
@@ -155,7 +154,7 @@ function loadMatchs() {
         .then(data => {
             console.log("respuesta del servidor:",data);
             console.log('Respuesta del servidor:exitosa');
-            var contenedor = document.getElementById('Pets-Info'); // Asegúrate de reemplazar 'miContenedor' con el ID real
+            var contenedor = document.getElementById('chats-matchs'); // Asegúrate de reemplazar 'miContenedor' con el ID real
             // Agregar la tabla al contenedor en lugar del cuerpo del documento
         
             // Datos de ejemplo
@@ -197,9 +196,10 @@ function loadMatchs() {
                         imgElement.addEventListener('click', function () {
                             sessionStorage.setItem('paginacion',0);
                             sessionStorage.setItem('matchPet', JSON.stringify(mascota));
-                            sessionStorage.setItem('userPropietaryMatch',JSON.stringify(mascota.userResponseDTO));
-                            var currentPet = JSON.parse(sessionStorage.getItem('currentPet'));
-                            ListOfUsers(sessionStorage.token,currentPet.animal,!currentPet.genero,currentPet.tamano,sessionStorage.paginacion);
+                           
+                            var UsuarioActual = JSON.parse(sessionStorage.getItem('AllDataUser'));
+
+                            loadMessages(UsuarioActual.id,mascota.id);
                             alert('La mascota ha sido cambiada por ' + mascota.nombre);
                             
                             window.location.href = 'chat.html';
@@ -231,9 +231,9 @@ function loadMatchs() {
 
 
 // Cargar mensajes en el chat
-function loadMessages() {
+function loadMessages(UsuarioActual,usuarioDestino) {
 
-    fetch(`http://localhost:8080/`, {
+    fetch(`http://localhost:8080/chat/getmessage?usuarioActual=${UsuarioActual}&usuarioDestino=${usuarioDestino}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -248,73 +248,8 @@ function loadMessages() {
         })
         .then(data => {
             console.log('Respuesta del servidor:exitosa');
-            var contenedor = document.getElementById('Pets-Info'); // Asegúrate de reemplazar 'miContenedor' con el ID real
-            // Agregar la tabla al contenedor en lugar del cuerpo del documento
+            var contenedor = document.getElementById('Chat-message'); 
         
-            // Datos de ejemplo
-            
-            var mascotas=data;
-         
-            // Crear una tabla
-            var tabla = document.createElement('table');
-            tabla.border = '1';
-            tabla.classList.add('box-table');
-
-            // Crear una fila de encabezado
-            var encabezado = tabla.createTHead();
-            var filaEncabezado = encabezado.insertRow();
-            ['ID','Foto', 'Nombre'].forEach(function (atributo) {
-                var celdaEncabezado = filaEncabezado.insertCell();
-                celdaEncabezado.textContent = atributo.toUpperCase();
-            });
-
-            // Crear filas de datos
-            var cuerpoTabla = tabla.createTBody();
-
-            mascotas.forEach(function (mascota) {
-                var fila = cuerpoTabla.insertRow();
-                ['id','foto', 'nombre'].forEach(function (atributo,index) {
-                        var celda = fila.insertCell();
-                        if(index!=1){
-                            celda.textContent = mascota[atributo];
-                        }
-                    if (index === 0) {
-                        celda.style.width = '50px';
-                    }
-                    if(index===1){
-                        var imgElement = document.createElement('img');
-                        imgElement.src =  mascota[atributo];
-                        imgElement.alt = 'Pet Image';
-            
-                        // Agregar un evento de clic a la imagen
-                        imgElement.addEventListener('click', function () {
-                            sessionStorage.setItem('paginacion',0);
-                            sessionStorage.setItem('currentPet', JSON.stringify(mascota));
-                            sessionStorage.setItem('userPropietary',JSON.stringify(mascota.userResponseDTO));
-                            var currentPet = JSON.parse(sessionStorage.getItem('currentPet'));
-                            ListOfUsers(sessionStorage.token,currentPet.animal,!currentPet.genero,currentPet.tamano,sessionStorage.paginacion);
-                            alert('La mascota ha sido cambiada por ' + mascota.nombre);
-                            
-                            window.location.href = 'adopcionesPendientes.html';
-                        });
-            
-                        celda.appendChild(imgElement);
-                        celda.style.width = '100px'; // Ancho de la columna 'foto'
-                        
-                        
-                    }
-                    if(index===2){
-                        celda.style.width='145px';
-                        var currentPet = JSON.parse(sessionStorage.getItem('currentPet'))
-                        if (currentPet && mascota['nombre'] === currentPet.nombre) {
-                            celda.style.color = 'red';
-                        }
-                    }
-                });
-            });
-
-            // Agregar la tabla al cuerpo del documento
-            contenedor.appendChild(tabla);
 
         })
         .catch(error => {
@@ -340,7 +275,7 @@ function sendMessage(Sender,SendTo,message) {
             'Content-Type': 'application/json',
             'Authorization': token
         }, 
-        body: JSON.stringify(message)
+        body: JSON.stringify(newMessage)
     })
         .then(response => {
             if (!response.ok) {
@@ -350,73 +285,9 @@ function sendMessage(Sender,SendTo,message) {
         })
         .then(data => {
             console.log('Respuesta del servidor:exitosa');
-            var contenedor = document.getElementById('Pets-Info'); // Asegúrate de reemplazar 'miContenedor' con el ID real
-            // Agregar la tabla al contenedor en lugar del cuerpo del documento
-        
-            // Datos de ejemplo
             
-            var mascotas=data;
          
-            // Crear una tabla
-            var tabla = document.createElement('table');
-            tabla.border = '1';
-            tabla.classList.add('box-table');
-
-            // Crear una fila de encabezado
-            var encabezado = tabla.createTHead();
-            var filaEncabezado = encabezado.insertRow();
-            ['ID','Foto', 'Nombre'].forEach(function (atributo) {
-                var celdaEncabezado = filaEncabezado.insertCell();
-                celdaEncabezado.textContent = atributo.toUpperCase();
-            });
-
-            // Crear filas de datos
-            var cuerpoTabla = tabla.createTBody();
-
-            mascotas.forEach(function (mascota) {
-                var fila = cuerpoTabla.insertRow();
-                ['id','foto', 'nombre'].forEach(function (atributo,index) {
-                        var celda = fila.insertCell();
-                        if(index!=1){
-                            celda.textContent = mascota[atributo];
-                        }
-                    if (index === 0) {
-                        celda.style.width = '50px';
-                    }
-                    if(index===1){
-                        var imgElement = document.createElement('img');
-                        imgElement.src =  mascota[atributo];
-                        imgElement.alt = 'Pet Image';
-            
-                        // Agregar un evento de clic a la imagen
-                        imgElement.addEventListener('click', function () {
-                            sessionStorage.setItem('paginacion',0);
-                            sessionStorage.setItem('currentPet', JSON.stringify(mascota));
-                            sessionStorage.setItem('userPropietary',JSON.stringify(mascota.userResponseDTO));
-                            var currentPet = JSON.parse(sessionStorage.getItem('currentPet'));
-                            ListOfUsers(sessionStorage.token,currentPet.animal,!currentPet.genero,currentPet.tamano,sessionStorage.paginacion);
-                            alert('La mascota ha sido cambiada por ' + mascota.nombre);
-                            
-                            window.location.href = 'adopcionesPendientes.html';
-                        });
-            
-                        celda.appendChild(imgElement);
-                        celda.style.width = '100px'; // Ancho de la columna 'foto'
-                        
-                        
-                    }
-                    if(index===2){
-                        celda.style.width='145px';
-                        var currentPet = JSON.parse(sessionStorage.getItem('currentPet'))
-                        if (currentPet && mascota['nombre'] === currentPet.nombre) {
-                            celda.style.color = 'red';
-                        }
-                    }
-                });
-            });
-
-            // Agregar la tabla al cuerpo del documento
-            contenedor.appendChild(tabla);
+         
 
         })
         .catch(error => {
